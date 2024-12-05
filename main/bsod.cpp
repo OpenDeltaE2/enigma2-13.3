da41f4debafa3a16feed46bcff47b65418b4c9ea
Open Delta Team <86120617+OpenDelta-Team@users.noreply.github.com>
2024-12-05 11:48:46 +0100

    Add files via upload

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -18,7 +18,7 @@
 
 static const char *crash_emailaddr =
 #ifndef CRASH_EMAILADDR
-	"the areadeltasat forum";
+	"the OpenPLi forum";
 #else
 	CRASH_EMAILADDR;
 #endif
@@ -150,7 +150,7 @@ void bsodFatal(const char *component)
 		strftime(tm_str, sizeof(tm_str), "%a %b %_d %T %Y", &tm);
 
 		fprintf(f,
-			"OpenDelta Enigma2 crash log\n\n"
+			"OpenPLi Enigma2 crash log\n\n"
 			"crashdate=%s\n"
 			"compiledate=%s\n"
 			"skin=%s\n"
