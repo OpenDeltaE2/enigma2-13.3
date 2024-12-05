49ab8e176ae9043b29ae0414b526fe3db1c983b1
Open Delta Team <86120617+OpenDelta-Team@users.noreply.github.com>
2024-12-05 10:42:11 +0100

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -39,7 +39,7 @@ class About(Screen):
 		AboutText += _("OE Version: (5.0.5)") + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
-        AboutText += _("Forum: https://areadeltasat.net") + "\n"
+                AboutText += _("Forum: https://areadeltasat.net") + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
